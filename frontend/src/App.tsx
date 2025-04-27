52f6f075ee9fd23014054eb5c9bcdd95295f896f
CP Payne <charlethical1@gmail.com>
2025-04-27 10:20:31 +0200

    Implement Authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-      <ToastContainer />
-    </>
-  );
+    return (
+        <>
+            <UserProvider>
+                <Navbar />
+                <Outlet />
+                <ToastContainer />
+            </UserProvider>
+        </>
+    );
 }
 
 export default App;
--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -1,9 +1,12 @@
+import axios from "axios";
+import { handleError } from "../Helpers/ErrorHandler";
 import { UserProfileToken } from "../Models/User";
 
-const api = "http://localhost:5167/api"
+
+const api = "http://localhost:5092/api/"
 
 export const loginAPI = async (username: string, password: string) => {
-    try{
+    try {
         const data = await axios.post<UserProfileToken>(api + "account/login", {
             username: username,
             password: password,
@@ -16,7 +19,7 @@ export const loginAPI = async (username: string, password: string) => {
 
 
 export const registerAPI = async (email: string, username: string, password: string) => {
-    try{
+    try {
         const data = await axios.post<UserProfileToken>(api + "account/register", {
             email: email,
             username: username,
