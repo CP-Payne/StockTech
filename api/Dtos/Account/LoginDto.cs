7a566a4314d0fb6f6bacb63192fb4d5dc9a0a74a
Charles <charlethical1@gmail.com>
2025-05-27 09:22:41 +0200

    refactor: change username login to email

--- api/Dtos/Account/LoginDto.cs
+++ api/Dtos/Account/LoginDto.cs
@@ -9,7 +9,8 @@ namespace api.Dtos.Account
     public class LoginDto
     {
         [Required]
-        public string Username { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
 
         [Required]
         public string Password { get; set; }
